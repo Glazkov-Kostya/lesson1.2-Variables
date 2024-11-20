c7acb3cad8dfb4757d7dd16b05c49b5407e70767
Glazkov-Kostya <JIaIIaJIyT@gmail.com>
2024-11-20 23:41:46 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,8 @@
+name = 'Kostya'
+print(name)
+name = 'Urban'
+print(name)
+date_of_birth = 'October 1996' # каждое последующее слово выделяем слешем
+print(date_of_birth)
+dateOfBirth = 'October 1996' # каждое последующее слово выделяем заглавной
+print(dateOfBirth)
